45f6a216396c99e4b07399851933274064e833f5
Abdul Rehman Butt <arbut4443@gmail.com>
2026-07-27 22:00:18 +0500

    deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,9 +15,8 @@ const startServer = async () => {
     startAppointmentScheduler();
     startAccountDeletionScheduler();
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     }
     catch (error) {
       console.error('Error connecting to MongoDB:', error);
